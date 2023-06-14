46f6971216bcbd109d60a6f072047770e327b31e
maximaizergit <maxim17shiglov08@gmail.com>
2023-06-15 00:09:12 +0300

    added ejs for rendering

--- server.js
+++ server.js
@@ -2,35 +2,49 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`Server started at port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(createPath("index"));
+  const title = "Home";
+  res.render(createPath("index"), { title });
 });
 
 app.get("/contacts", (req, res) => {
-  res.sendFile(createPath("contacts"));
+  const title = "Contacts";
+  const contacts = [
+    { name: "VK", link: "http://vk.com/ggshiglov" },
+    { name: "Telegramm", link: "https://t.me/MaximShiglov" },
+    { name: "GitHub", link: "https://github.com/maximaizergit" },
+  ];
+  res.render(createPath("contacts"), { contacts, title });
 });
 
 app.get("/posts", (req, res) => {
-  res.sendFile(createPath("contacts"));
+  const title = "Posts";
+  res.render(createPath("posts"), { title });
 });
 
 app.get("/posts/:id", (req, res) => {
-  res.sendFile(createPath("contacts"));
+  const title = "Post";
+  res.render(createPath("post"), { title });
 });
 
 app.get("/add-post", (req, res) => {
-  res.sendFile(createPath("contacts"));
+  const title = "Add post";
+  res.render(createPath("add-post"), { title });
 });
 
 app.use((req, res) => {
+  const title = "Error";
   res.statusCode = 404;
-  res.sendFile(createPath("error"));
+  res.render(createPath("error"), { title });
 });
